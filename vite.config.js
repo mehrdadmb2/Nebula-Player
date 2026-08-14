1d8303f7d81d3582596f637cc99a2d7dac104990
Μιθριδάτης <game.developer.mb@gmail.com>
2026-08-14 12:20:11 +0330

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+});
