6ee177684f67693966a154d457661936f3584fc3
Μιθριδάτης <game.developer.mb@gmail.com>
2026-08-14 15:06:34 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Nebula-Player/'  // ← این خط را اضافه کنید (نام ریپو را دقیقاً بنویسید)
+  base: './',   // ← این خط را به './' تغییر دهید
 });
