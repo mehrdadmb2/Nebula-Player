4b3e8b7475f4a209e32d641c27ee2ec68724720e
Μιθριδάτης <game.developer.mb@gmail.com>
2026-08-14 12:28:59 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,3 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
-});
+export default defineConfig({ plugins: [react()] });
